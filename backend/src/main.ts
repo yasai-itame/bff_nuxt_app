e1bfb458efc8f63f0dee8dd1960e030550c29ac1
yuukishouda <shoudathird@outlook.jp>
2025-03-09 17:25:13 +0900

    fix main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { Server } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const server: Server = await app.listen(process.env.PORT ?? 4000);
-  return server;
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
