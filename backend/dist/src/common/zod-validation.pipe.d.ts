932880117d38e9ee2f610e2b62db8766915ae9b3
yuukishouda <shoudathird@outlook.jp>
2025-03-10 08:05:58 +0900

    fix: dist Delete

--- backend/dist/src/common/zod-validation.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from "@nestjs/common";
-import { ZodSchema } from "zod";
-export declare class ZodValidationPipe implements PipeTransform {
-    private readonly schema;
-    constructor(schema: ZodSchema<any>);
-    transform(value: any): any;
-}
