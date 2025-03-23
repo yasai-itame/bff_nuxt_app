556d8c2193d9dce5f3307ecd5ffa31f8554d0f75
yuukishouda <shoudathird@outlook.jp>
2025-03-24 08:11:41 +0900

    fix Add github action and other adjust

--- backend/prepare-schema.ts
+++ backend/prepare-schema.ts
@@ -1,13 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-// envファイル読み込みで必要
-require("dotenv").config();
-
 const provider = process.env.DATABASE_PROVIDER || "mysql";
-
-console.log("🔍 DATABASE_PROVIDER from env:", process.env.DATABASE_PROVIDER);
-console.log("🔍 Selected provider:", provider);
 
 const schemaTemplatePath = path.join(__dirname, "prisma/schema.template.prisma");
 const schemaPath = path.join(__dirname, "prisma/schema.prisma");
