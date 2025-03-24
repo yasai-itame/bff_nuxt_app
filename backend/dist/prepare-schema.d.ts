d6585e90a5b3ea3caf176fc9f262af5356c7a189
yuukishouda <shoudathird@outlook.jp>
2025-03-24 14:01:12 +0900

    fix

--- backend/dist/prepare-schema.d.ts
+++
@@ -1,7 +0,0 @@
-declare const fs: any;
-declare const path: any;
-declare const provider: string;
-declare const schemaTemplatePath: any;
-declare const schemaPath: any;
-declare const schemaTemplate: any;
-declare const updatedSchema: any;
--- backend/dist/prepare-schema.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const provider = process.env.DATABASE_PROVIDER || "mysql";
-const schemaTemplatePath = path.join(__dirname, "prisma/schema.template.prisma");
-const schemaPath = path.join(__dirname, "prisma/schema.prisma");
-const schemaTemplate = fs.readFileSync(schemaTemplatePath, "utf8");
-const updatedSchema = schemaTemplate.replace("__PROVIDER__", provider);
-fs.writeFileSync(schemaPath, updatedSchema);
-//# sourceMappingURL=prepare-schema.js.map
