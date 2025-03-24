7291b71e4cd31b70933c2486965c7b233a60fd2b
yuukishouda <shoudathird@outlook.jp>
2025-03-25 08:33:08 +0900

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
-  await app.listen(process.env.PORT ?? 4000);
+  // await app.listen(process.env.PORT ?? 4000);
+  await app.init();
+  return app.getHttpAdapter().getInstance();
 }
 bootstrap();
