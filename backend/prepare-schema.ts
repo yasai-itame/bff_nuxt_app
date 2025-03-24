fb7a4d0912338b84b1d64d0bec6f6627de7e3d1d
yuukishouda <shoudathird@outlook.jp>
2025-03-24 10:27:19 +0900

    fix Add migrationfile

--- backend/prepare-schema.ts
+++ backend/prepare-schema.ts
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const provider = process.env.DATABASE_PROVIDER || "mysql";
+const provider = process.env.DATABASE_PROVIDER;
+const database = process.env.DATABASE_URL;
 
 const schemaTemplatePath = path.join(__dirname, "prisma/schema.template.prisma");
 const schemaPath = path.join(__dirname, "prisma/schema.prisma");
 
 const schemaTemplate = fs.readFileSync(schemaTemplatePath, "utf8");
 
-const updatedSchema = schemaTemplate.replace("__PROVIDER__", provider);
+const updatedSchema = schemaTemplate.replace("__PROVIDER__", provider).replace("__DATABASE__", database);
 
 fs.writeFileSync(schemaPath, updatedSchema);
